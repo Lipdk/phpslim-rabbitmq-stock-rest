f58ed5ddc9648f84098ea79e516bc0257797d011
Luis Monteiro <lmonteiro@w-4.ch>
2023-04-29 12:39:23 -0300

    Added stock route

--- app/routes.php
+++ app/routes.php
@@ -2,13 +2,35 @@
 
 declare(strict_types=1);
 
-use App\HelloController;
+use App\Controllers\HelloController;
+use App\Controllers\StockController;
 use Slim\App;
+use App\Models\Bootstrap;
+use App\Models\User;
+use Slim\Interfaces\RouteCollectorProxyInterface as Group;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 
 return function (App $app) {
+//    $container = $app->getContainer();
+//    Bootstrap::load($container);
+
     // unprotected routes
     $app->get('/hello/{name}', HelloController::class . ':hello');
 
     // protected routes
     $app->get('/bye/{name}', HelloController::class . ':bye');
+
+    $app->get('/stock', StockController::class . ':quote');
+
+    $app->get('/users', function (Request $request, Response $response) {
+        $users = User::all()->toArray();
+        $response->getbody()->write(json_encode($users));
+        return $response;
+    });
+
+//    $app->group('/users', function (Group $group) use ($container) {
+//        $group->get('', ListUsersAction::class);
+//        $group->get('/{id}', ViewUserAction::class);
+//    });
 };
